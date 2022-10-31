50bbe136646709afd50d1838969a2eb487c56f5e
Juan Francisco Romay Sieira <juan.sieira@xercode.es>
2022-10-31 11:06:06 +0100

    Mod - Texto para el botón multiidioma

--- Koha/Plugin/Es/Xercode/SpecialReserves/js/specialreserves.js
+++ Koha/Plugin/Es/Xercode/SpecialReserves/js/specialreserves.js
@@ -4,7 +4,7 @@ if ( $('body#opac-detail').length ){
     var biblionumber =  $('#catalogue_detail_biblio').data('biblionumber');
     $.getJSON( "/canspecialreserve.pl?biblionumber="+biblionumber+'&lang='+lang, function( data ) {
         if (data.result == 1){
-            $('#action').append('<li><a class="specialreserve btn btn-link btn-lg" href="/specialreserve.pl?biblionumber='+biblionumber+'"><i class="fa fa-fw fa-bookmark" aria-hidden="true"></i> Place Special hold</a></li>');
+            $('#action').append('<li><a class="specialreserve btn btn-link btn-lg" href="/specialreserve.pl?biblionumber='+biblionumber+'"><i class="fa fa-fw fa-bookmark" aria-hidden="true"></i> '+data.button_text+'</a></li>');
         }
     });
 }
