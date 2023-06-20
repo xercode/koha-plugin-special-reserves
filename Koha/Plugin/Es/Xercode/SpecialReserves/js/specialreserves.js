50ad9e0ad13482fedd203d9322328985992357d8
Adolfo Rodríguez Taboada <planetainquietante@gmail.com>
2023-06-20 09:30:18 +0200

    Fix specialreserves.js
    Corregido el enlace para realizar reservas especiales

--- Koha/Plugin/Es/Xercode/SpecialReserves/js/specialreserves.js
+++ Koha/Plugin/Es/Xercode/SpecialReserves/js/specialreserves.js
@@ -4,7 +4,7 @@ if ( $('body#opac-detail').length ){
     var biblionumber =  $('#catalogue_detail_biblio').data('biblionumber');
     $.getJSON( "/reserve/can-special-reserves.pl?biblionumber="+biblionumber+'&lang='+lang, function( data ) {
         if (data.result == 1){
-            $('#action').append('<li><a class="specialreserve btn btn-link btn-lg" href="/specialreserve.pl?biblionumber='+biblionumber+'"><i class="fa fa-fw fa-bookmark" aria-hidden="true"></i> '+data.button_text+'</a></li>');
+            $('#action').append('<li><a class="specialreserve btn btn-link btn-lg" href="/reserve/opac-special-reserves.pl?biblionumber='+biblionumber+'"><i class="fa fa-fw fa-bookmark" aria-hidden="true"></i> '+data.button_text+'</a></li>');
         }
     });
 }
