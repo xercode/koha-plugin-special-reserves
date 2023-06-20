68a5dc05ae0a37c42e4ab91a29b4259b785575d5
Adolfo Rodríguez Taboada <planetainquietante@gmail.com>
2023-06-20 09:25:56 +0200

    Fix specialreserves.js
    Corregida llamada a script para comprobar si se pueden hacer reservas

--- Koha/Plugin/Es/Xercode/SpecialReserves/js/specialreserves.js
+++ Koha/Plugin/Es/Xercode/SpecialReserves/js/specialreserves.js
@@ -2,7 +2,7 @@ if ( $('body#opac-detail').length ){
 
     var lang = $('html').attr('lang');
     var biblionumber =  $('#catalogue_detail_biblio').data('biblionumber');
-    $.getJSON( "/canspecialreserve.pl?biblionumber="+biblionumber+'&lang='+lang, function( data ) {
+    $.getJSON( "/reserve/can-special-reserves.pl?biblionumber="+biblionumber+'&lang='+lang, function( data ) {
         if (data.result == 1){
             $('#action').append('<li><a class="specialreserve btn btn-link btn-lg" href="/specialreserve.pl?biblionumber='+biblionumber+'"><i class="fa fa-fw fa-bookmark" aria-hidden="true"></i> '+data.button_text+'</a></li>');
         }
